baf9b90baf64a28fbdb871f908c0487a4e911c6b
Jeremy Rothschild <jerben.rothschild@gmail.com>
2018-05-28 21:39:08 +0000

    euler_boundary.py had to be edited because it was the wrong file I copied.

--- euler_boundary.py
+++ euler_boundary.py
@@ -1,93 +1,98 @@
 import sys
 import numpy as np
 import os
-import euler_boundary as bd
 from skimage import io
-#import plot_diffusion as pd
 
-def main(sim, load, D):
-    cwd = os.getcwd() + "/Sim_" + sim + str(D) #remote use
-    load_dir = os.getcwd() + '/ChanLab/'
-    #cwd = $HOME #on Scinet
-    if not os.path.exists(cwd):
-        os.makedirs(cwd)
-        
-    #parameters
-    count = '/lastTime_seconds.npy'
-    size = 301
-    dx = 1; dy = 1; dz = 1 #1 micrometer
-    time = 3600*4
-    dt = 30 #Time steps of 30 seconds
-    vis = D #Diffusion coefficient
-    v_radius = int(10./dx) #vessel radius in units of dx
-    h_radius = int(1./dx) #hole radius in units of dx
+cwd = os.getcwd() + "/Sim"
+
+def dirichlet(size, hole_radius, vessel_radius):
+    """
+    Function that saves a 3D array of the Dirichlet boundary sources. Note that for now sources are all the same value
+
+    Args:
+        size(int): The size of the 3D array along one dimension
+    Returns:
+        bc_D(array[size,size,size]): Array of source terms in the model
+    """
+    bc_D = np.zeros((size,size,size))
+
+    bc_D[0,:,:] = 0; bc_D[:,0,:] = 0; bc_D[:,:,0] = 0
+    bc_D[-1,:,:] = 0; bc_D[:,-1,:] = 0; bc_D[:,:,-1] = 0
+
+    #------HOLE at 150,160,150------
+    bc_D[150,160,150] = 1.0
+    #-------------------------------
+
+    return bc_D
+
+def neumann(size, hole_radius, vessel_radius):
+    """
+    Function  that saves a 3D array of the Neumann boundary locations
+
+    Args:
+        size(int): The size of the 3D array along one dimension
+    Returns:
+        uref(array[size,size,size]): Array of locations that have a Neumann boundary conditions
+    """
+
+    uref = np.zeros((size,size,size))
+
+    radius = 10.
+    for i in range(0,size):
+        for j in range(0,size):
+            for k in range(0,size):
+                if ((j-int(size/2))**2 + (i-int(size/2))**2 <= vessel_radius**2):
+                    uref[i,j,k] = 0.0
+                else:
+                    uref[i,j,k] = 1.0
+    uref[150,160,150] = 1.0
+
+    return uref
+
+def tiff_load(filename):
+    """
+    Function that loads a TIFF 3D image and outputs its 3D array
+
+    Args:
+        filename(string): the name of the file to load
+    Returns:
+        The array
+    """
     
-    file = open(cwd + "/params.txt","w")
-    if load != True:
-        file.write("Created from model in euler_boudary\n")
-        file.write("size =" + str(size)+"\n")
-        file.write("v_radius = "+ str(v_radius) +" #vessel radius in units of dx\n")
-        file.write("h_radius = "+ str(h_radius) +" #hole radius in units of dx\n")
+    return io.imread(filename)
+
+def concentration_time(time):
+    """
+    Function that calculates the change in concentration in the blood vessel as a function of time
+
+    Args:
+        time(int): time in seconds
+    Returns:
+        The concentration
+    """
+    #return 1.0
+    return 0.7521*np.exp(-1.877*time) + 0.2479*np.exp(-0.1353*time)
+
+def main(action='load', size=301, hole_radius=1, vessel_radius=10, cwd = cwd, fileN='none', fileD='none', file_domain='none'):
+    #====================Grid constuction=====================
+    if action == 'load':
+        bc_D = tiff_load(fileD).astype(float)
+        bc_N = 1 - tiff_load(fileN).astype(float) + bc_D
+        uref_D = tiff_load(file_domain).astype(float)
+
     else:
-        file.write("Loaded boundary conditions\n")
-    file.write("dx = "+ str(dx) +"; dy = "+ str(dy) +"; dz = "+ str(dz) +" #1 micrometer\n")
-    file.write("time = "+ str(time)+"\n")
-    file.write("dt = "+ str(dt) +" #Time steps of 30 seconds\n")
-    file.write("vis = "+ str(vis) +" #Diffusion coefficient\n")
-    file.close() 
-    
-    
-    #boundaries
-    if not os.path.exists(cwd + count):
-        #If we're using tiff images
-        if load == True:
-            bd.main('load', size=size, hole_radius=h_radius, vessel_radius=v_radius, cwd=cwd, fileN=load_dir + 'UT16-T-stack3-Sept10_iso_vesthresh-cropped.tif', fileD= load_dir + 'UT16-T-stack3-Sept10_iso_gaps-cropped.tif', file_domain= load_dir + 'UT16-T-stack3-Sept10_iso_tissueboundary-cropped.tif')
-            #If we're using boundaries from euler_boundary
-        else:
-            bd.main('create', size=size, hole_radius=h_radius, vessel_radius=v_radius, cwd=cwd)
+        bc_N = neumann(size, hole_radius, vessel_radius)
+        bc_D = dirichlet(size, hole_radius, vessel_radius)
+        #Location of Internal and Boundary Dirichlets conditions
+        uref_D = np.zeros((size,size,size)) + 1
+        uref_D[0,:,:] = 0.0;  uref_D[:,0,:] = 0.0; uref_D[:,:,0] = 0.0
+        uref_D[-1,:,:] = 0.0; uref_D[:,-1,:] = 0.0; uref_D[:,:,-1] = 0.0
 
-    neumann = np.load(cwd + "/Neumann.npy") #Neumann
-    domain = np.load(cwd + "/Domain.npy") #domain
-    dirichlet = np.load(cwd + "/Dirichlet.npy") #location of fixed concentration
-    
-    u = np.zeros((domain.shape[0], domain.shape[1], domain.shape[2])) #solution
-    un = np.zeros((domain.shape[0], domain.shape[1], domain.shape[2])) #temp location for solution
-    ijk = (np.linspace(1, domain.shape[0]-2, domain.shape[0]-2)).astype(int) #part of domain
-    """
-    neum_i = np.load(cwd + "/neum_i.npy"); neum_ip = np.load(cwd + "/neum_ip.npy"); neum_im = np.load(cwd + "/neum_im.npy")
-    nuem_j = np.load(cwd + "/neum_j.npy"); neum_jp = np.load(cwd + "/neum_jp.npy"); neum_jm = np.load(cwd + "/neum_jm.npy")
-    neum_k = np.load(cwd + "/neum_k.npy"); neum_kp = np.load(cwd + "/neum_kp.npy"); neum_km = np.load(cwd + "/neum_km.npy")
-    """
-    
-    if not os.path.exists(cwd + count): #saves initial and creates lastTime_seconds.npy to run simulations. Basically checks at what step we're at
-        np.save(cwd + count, np.asarray(0))
-        np.save(cwd + "/diff_0min.npy", dirichlet*bd.concentration_time(0))
-        
-    initial = np.load(cwd + count)
-    u = np.load(cwd + "/diff_" + str(initial/60) + "min" +".npy")
+    #Main Arrays
+    np.save(cwd + "/Neumann", bc_N)
+    np.save(cwd + "/Domain", uref_D-bc_D)
+    np.save(cwd + "/Dirichlet", bc_D)
 
-    #================Euleur's method============================
 
-    for i in range(int(initial/dt)+1,time/dt+1): #run simulation from time
-        un = u
-        print i
-        u[:,:,:] = un[:,:,:]
-        
-        u[ijk,:,:] += neumann[ijk,:,:]*( vis*dt*neumann[ijk+1,:,:]*( un[ijk+1,:,:]-un[ijk,:,:] ) + vis*dt*neumann[ijk-1,:,:]*( un[ijk-1,:,:]-un[ijk,:,:] ))/(dx**2)
-        
-        u[:,ijk,:] += neumann[:,ijk,:]*( vis*dt*neumann[:,ijk+1,:]*( un[:,ijk+1,:]-un[:,ijk,:] ) + vis*dt*neumann[:,ijk-1,:]*( un[:,ijk-1,:]-un[:,ijk,:] ))/(dy**2)
-               
-        u[:,:,ijk] += neumann[:,:,ijk]*( vis*dt*neumann[:,:,ijk+1]*( un[:,:,ijk+1]-un[:,:,ijk] ) + vis*dt*neumann[:,:,ijk-1]*( un[:,:,ijk-1]-un[:,:,ijk] ))/(dz**2)
-
-        u = u*domain + bd.concentration_time(i*dt/3600)*dirichlet
-        
-        if i in range(0,time,20):
-            np.save(cwd + "/diff_"+str(i*dt/60)+"min", u)
-            np.save(cwd + "/" + count, np.asarray(i*dt))
-            print np.load(cwd + count)
-            #io.imsave(cwd + "/diff_"+str(i*dt/60)+"min.tif", u)
-            
-        print np.sum(u)
-        
-if __name__ == "__main__":
-    main(sim='Syed', load=True, D=0.01)
+#main('load', size=301, hole_radius=1, vessel_radius=10, cwd=cwd, fileN=os.getcwd() + '/ChanLab/' + 'UT16-T-stack3-Sept10_iso_vesthresh-cropped.tif', fileD= os.getcwd() + '/ChanLab/' + 'UT16-T-stack3-Sept10_iso_gaps-cropped.tif', file_domain= os.getcwd() + '/ChanLab/' + 'UT16-T-stack3-Sept10_iso_tissueboundary-cropped.tif')
+#main('create', size=301, hole_radius=1, vessel_radius=10, cwd=cwd)
