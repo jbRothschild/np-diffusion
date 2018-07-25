18660e02ab32bd8231fcd7261128cd1476da8584
jbRothschild <jerben.rothschild@gmail.com>
2018-07-25 13:05:44 -0400

    Fixed the issue. Wasn't as bad as I thought tbh, it was a problem with how argparse would store the command line input (vis) as a list.

--- model/diffusion.py
+++ model/diffusion.py
@@ -12,6 +12,7 @@ def diffusion(u, un, ijk, diffusion_location, vis, dt, dx, dy, dz, mod):
      Returns:
          None
      """
+     print vis
      u[ijk,:,:] += diffusion_location[ijk,:,:]*( vis*dt*diffusion_location[ijk+1,:,:]*( un[ijk+1,:,:]-un[ijk,:,:] ) + vis*dt*diffusion_location[ijk-1,:,:]*( un[ijk-1,:,:]-un[ijk,:,:] ))/(dx**2)
 
      u[:,ijk,:] += diffusion_location[:,ijk,:]*( vis*dt*diffusion_location[:,ijk+1,:]*( un[:,ijk+1,:]-un[:,ijk,:] ) + vis*dt*diffusion_location[:,ijk-1,:]*( un[:,ijk-1,:]-un[:,ijk,:] ))/(dy**2)
