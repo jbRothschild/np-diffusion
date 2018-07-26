3c2254a8ec547653a2552f93c05e61fe22708717
jbRothschild <jerben.rothschild@gmail.com>
2018-07-26 11:00:51 -0400

    make->cmake

--- niagara_submit.sh
+++ niagara_submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=40
-#SBATCH --time=2:00:00
+#SBATCH --time=6:00:00
 #SBATCH --job-name np_diffusion
 
 #run this code using jbroths:~$ sbatch *script_name.sh*
