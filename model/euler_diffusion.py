a32e9dd58f6f0caa9a4b9d166a3a028455c0c5f3
jbRothschild <jerben.rothschild@gmail.com>
2018-09-03 11:24:10 -0400

    Adding holes_model.py

--- model/euler_diffusion.py
+++ model/euler_diffusion.py
@@ -91,4 +91,4 @@ def main(sim_name, load, D_coeff):
     print toc-tic, "sec for Euler diffusion !"
 
 if __name__ == "__main__":
-    main(sim_name='holes5k_diffusion_', load=True, D_coeff=vars(args)['D'])
+    main(sim_name='sim_holes5k_diffusion_', load=True, D_coeff=vars(args)['D'])
