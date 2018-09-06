99d30cf07130f5c69fe835bfadd0d217afc5d723
jbrothschild <jerben.rothschild@gmail.com>
2018-09-06 15:58:31 -0400

    Code now works. We'll see how it does on the full data (could be long)

--- model/homogeneity.py
+++ model/homogeneity.py
@@ -20,9 +20,9 @@ def rev_diffusion_dist(data, vessel, domain = []):
     #Pseudo reverse diffusion
     data_temp = data
     for t in np.arange(0,20):
-        for i in np.arange(1,data.shape[0]):
-            for j in np.arange(1,data.shape[1]):
-                for k in np.arange(1,data.shape[2]):
+        for i in np.arange(1,data.shape[0]-1):
+            for j in np.arange(1,data.shape[1]-1):
+                for k in np.arange(1,data.shape[2]-1):
                     if vessel[i,j,k] == 0:
                         temp = data[i,j,k]/6
                         data_temp[i+1,j,k] = temp
@@ -35,9 +35,9 @@ def rev_diffusion_dist(data, vessel, domain = []):
 
     #Find distribution of particles on vessel surface
     dist = []
-    for i in np.arange(0,data.shape[0]+1):
-        for j in np.arange(0,data.shape[1]+1):
-            for k in np.arange(0,data.shape[2]+1):
+    for i in np.arange(0,data.shape[0]):
+        for j in np.arange(0,data.shape[1]):
+            for k in np.arange(0,data.shape[2]):
                 if vessel[i,j,k] > 0 and data[i,j,k] > 0.0:
                     dist.append(data[i,j,k])
     return dist
@@ -54,21 +54,21 @@ def main(sim_name, domainfile, sourcefile, datafile, method, use_skel):
 
     if not os.path.exists(sim_name + '/skeleton_vessel.npy'):
         skel = morph.skeletonize_3d(vessel)
-        np.save(sim + '/skeleton_vessel.npy', skel)
+        np.save(sim_name + '/skeleton_vessel.npy', skel)
     if use_skel == True:
         vessel = np.load(sim_name + '/skeleton_vessel.npy')
 
     H_score = []
 
-    for i in np.arange(0,data.shape):
+    for i in np.arange(0,len(datafile)):
         data = np.load(sim_name + datafile[i])
         distribution = method(data, vessel, domain-source)
-        H_scores.append(heterogen_score(distribution))
+        H_score.append(heterogen_score(distribution))
 
-    print H_scores
+    print H_score
 
     return 0
 
 if __name__ == "__main__":
-    main(sim_name = '../data/try', domainfile = 'diffusion_try.npy', sourcefile = 'source_try.npy', datafile = '/data_try.npy', method = rev_diffusion_dist, use_skel=False)
-    #main(sim_name = '../data/holesUT16', domainfile = '/diffusion_location.npy', sourcefile = '/source_location.npy', datafile = ['diff_holes00300.npy','diff_holes00500.npy','diff_holes01000.npy','diff_holes03000.npy','diff_holes05000.npy','diff_holes10000.npy','diff_holes30000.npy','diff_holes50000.npy',], method = rev_diffusion_dist, use_skel=False)
+    #main(sim_name = '../data/try', domainfile = '/pd_diffusion.npy', sourcefile = '/pd_source.npy', datafile = ['/pd_3000.npy','/pd_5000.npy','/pd_10000.npy','/pd_30000.npy','/pd_50000.npy'], method = rev_diffusion_dist, use_skel=False)
+    main(sim_name = '../data/holesUT16', domainfile = '/diffusion_location.npy', sourcefile = '/source_location.npy', datafile = ['diff_holes00300.npy','diff_holes00500.npy','diff_holes01000.npy','diff_holes03000.npy','diff_holes05000.npy','diff_holes10000.npy','diff_holes30000.npy','diff_holes50000.npy',], method = rev_diffusion_dist, use_skel=False)
