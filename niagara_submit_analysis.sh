0ac4f14fc58b34c25cd56fe65318f9e017e2ae9f
jbrothschild <jerben.rothschild@gmail.com>
2018-09-06 14:36:39 -0400

    reworked submit script

--- niagara_submit_analysis.sh
+++ niagara_submit_analysis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=40
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 #SBATCH --job-name np_diffusion
 
 #run this code using jbroths:~$ sbatch *script_name.sh*
